3fb2d2a049fb59ff6c1e0604609d2dbe9f3da127
kartamyshev <kartamyshev.k@gmail.com>
2025-01-27 15:10:54 +0100

    move all deployment logic under prepare.sh script

--- source/scripts/prepare.sh
+++ source/scripts/prepare.sh
@@ -2,3 +2,19 @@
 
 node create-pdf.js
 cp -r ./assets/* ../assets/ && cp ./index.html ../
+
+if [ "$1" == '-p' ]; then
+    if [ -z "$2" ]; then
+        echo "Error: No commit message provided."
+        echo "Usage: ./prepare.sh \"Your commit message\""
+        exit 1
+    fi
+    version=$(node -p "require('./package.json').version")
+    commit_message="$2"
+
+    # echo "[$version] - $commit_message"
+
+    git add -A
+    git commit -m "$commit_message"
+    git push origin master
+fi
