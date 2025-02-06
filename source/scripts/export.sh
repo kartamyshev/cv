550b440a5345f5c5952f7383d1a98841bb33e1f7
kartamyshev <kartamyshev.k@gmail.com>
2025-02-06 16:36:10 +0100

    [4.0.0] - Remove elegant theme

--- source/scripts/export.sh
+++ source/scripts/export.sh
@@ -1,12 +1,8 @@
-THEME="elegant"
+THEME="kartamyshev"
 
 if [ -n "$npm_config_argv" ]; then
     ARGS=$(node -pe 'JSON.parse(process.env.npm_config_argv).original.slice(1).join(" ")')
     set -- $ARGS
-fi
-
-if [ -n "$1" ]; then
-    THEME="$1"
 fi
 
 resume export index.html --theme "$THEME"
--- source/scripts/serve.sh
+++ source/scripts/serve.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-THEME="elegant"
+THEME="kartamyshev"
 
 if [ -n "$npm_config_argv" ]; then
     ARGS=$(node -pe 'JSON.parse(process.env.npm_config_argv).original.slice(1).join(" ")')
     set -- $ARGS
-fi
-
-if [ -n "$1" ]; then
-    THEME="$1"
 fi
 
 resume serve --port 8082 --theme "$THEME"
