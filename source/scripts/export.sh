8e4c6b41a833b45e9dfae155587005f99bbe1405
kartamyshev <kartamyshev.k@gmail.com>
2025-02-12 20:37:40 +0100

    Remove redundant scripts

--- source/scripts/export.sh
+++
@@ -1,8 +0,0 @@
-THEME="kartamyshev"
-
-if [ -n "$npm_config_argv" ]; then
-    ARGS=$(node -pe 'JSON.parse(process.env.npm_config_argv).original.slice(1).join(" ")')
-    set -- $ARGS
-fi
-
-resume export index.html --theme "$THEME"
